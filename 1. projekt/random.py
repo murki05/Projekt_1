a5a30372efc7625691c8c094b7f0a1c0bc020686
murki05 <144987627+murki05@users.noreply.github.com>
2023-09-21 09:08:24 +0200

    Update random.py
    Rengeteg hibajavítás

--- 1. projekt/random.py
+++ 1. projekt/random.py
@@ -5,41 +5,45 @@ while True:
     print("2. Véletlen szöveg generálása")
     print("3. Számok ellenőrzése")
     print("4. Szöveg ellenőrzése")
+    print("5. Kilépés")
 
-    valasz = input("Választott lehetőség (1/2/3/4): ")
+    choice = input("Választott lehetőség (1/2/3/4/5): ")
 
-    if valasz == "1":
-        also_hatar = int(input("Add meg az alsó határt: "))
-        felso_hatar = int(input("Add meg a felső határt: "))
-        szamol = int(input("Add meg a számok darabszámát: "))
-        szamok = [random.randint(also_hatar, felso_hatar) for _ in range(szamol)]
+    if choice == "1":
+        lower_limit = int(input("Add meg az alsó határt: "))
+        upper_limit = int(input("Add meg a felső határt: "))
+        count = int(input("Add meg a számok darabszámát: "))
+        numbers = [random.randint(lower_limit,upper_limit) for _ in range(count)]
         with open("ki.txt", "w") as f:
-            file.write(";".join(map(str, szamok)))
+            f.write(";".join(map(str,numbers)))
         print("Számok generálva és kiírva a ki.txt-be.")
 
-    elif valasz == "2":
-        szamol = int(input("Add meg a szövegek darabszámát: "))
-        szovegek = [''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(random.randint(1,20)))for _ in range(szamol)]
-        with open("ki.txt","w") as f:
-            file.write(";".join(szovegek))
+    elif choice == "2":
+        count = int(input("Add meg a szövegek darabszámát: "))
+        texts = [''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(random.randint(1,20)))for _ in range(count)]
+        with open("ki.txt", "w") as f:
+            f.write(";".join(texts))
         print("Szövegek generálva és kiírva a ki.txt-be.")
 
-    elif valasz == "3":
-        also_hatar = int(input("Add meg az alsó határt: "))
-        felso_hatar = int(input("Add meg a felső határt: "))
+    elif choice == "3":
+        lower_limit = int(input("Add meg az alsó határt: "))
+        upper_limit = int(input("Add meg a felső határt: "))
         with open("ki.txt","r") as f:
-            data = file.read()
-        szamok = [int(num) for num in data.split(";")]
-        if all(also_hatar <= num <= felso_hatar for num in szamok):
+            data = f.read()
+        numbers = [int(num) for num in data.split(";")]
+        if all(lower_limit <= num <= upper_limit for num in numbers):
             print("A számok megfelelnek a feltételeknek.")
         else:
             print("A számok nem felelnek meg a feltételeknek.")
 
     elif choice == "4":
         with open("ki.txt","r") as f:
-            data = file.read()
-        szovegek = data.split(";")
-        if all (1 <= len(text) <= 20 for text in szovegek):
+            data = f.read()
+        texts = data.split(";")
+        if all (1 <= len(text) <= 20 for text in texts):
             print("A szövegek megfelelnek a feltételeknek.")
         else:
             print("A szövegek nem felelnek meg a feltételeknek.")
+
+    elif choice == "5":
+        break
