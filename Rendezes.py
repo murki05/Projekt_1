6c3b78bfca1bc2fd715980c86d8339daead49afd
murki05 <144987627+murki05@users.noreply.github.com>
2023-10-01 20:04:38 +0200

    Update Rendezes.py
    hibajavitások

--- Rendezes.py
+++ Rendezes.py
@@ -1,121 +1,54 @@
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+def read_file(filename):
+try:
+with open(filename, 'r') as file:
+lines = file.readlines()
+return [line.strip() for line in lines]
+except FileNotFoundError:
+print("A megadott fájl nem található.")
+return []
 
-def read_data_from_file(ki.txt):
-    data = []
-    try:
-        with open(ki.txt, 'r') as file:
-            for line in file:
-                data.append(line.strip())
-        return data
-    except FileNotFoundError:
-        print(f"A(z) {ki.txt} fájl nem található.")
-        return None
+def is_numeric(value):
+try:
+float(value)
+return True
+except ValueError:
+return False
 
-def simple_swap_sort(arr, reverse=False):
-    n = len(arr)
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
-            if (not reverse and arr[j] > arr[j + 1]) or (reverse and arr[j] < arr[j + 1]):
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-def bubble_sort(arr, reverse=False):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if (not reverse and arr[j] > arr[j + 1]) or (reverse and arr[j] < arr[j + 1]):
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-def insertion_sort(arr, reverse=False):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and ((not reverse and arr[j] > key) or (reverse and arr[j] < key)):
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
-def selection_sort(arr, reverse=False):
-    for i in range(len(arr)):
-        min_idx = i
-        for j in range(i + 1, len(arr)):
-            if (not reverse and arr[j] < arr[min_idx]) or (reverse and arr[j] > arr[min_idx]):
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
-def custom_sort(arr, reverse=False):
-    pass
+def bubble_sort(data, reverse=False):
+n = len(data)
+for i in range(n):
+for j in range(0, n - i - 1):
+if (data[j] > data[j + 1] and not reverse) or (data[j] < data[j + 1] and reverse):
+data[j], data[j + 1] = data[j + 1], data[j]
 
 def main():
-    filename = "ki.txt"
-    data = read_data_from_file(filename)
+filename = input("Kérem, adja meg a fájl nevét: ")
+data = read_file(filename)
 
-    if data is None:
-        return
+if not data:
+return
 
-    is_numeric = all(is_number(item) for item in data)
+if all(is_numeric(item) for item in data):
+data = [float(item) for item in data]
+numeric = True
+else:
+numeric = False
+print("Az adatok nem mind számok, szöveges adatok is vannak.")
 
-    if is_numeric:
-        data = [float(item) for item in data]
-    else:
-        data = [str(item) for item in data]
+print("Az adatok:", data)
 
-    print("Az adatok beolvasása után:")
-    print(data)
+while True:
+choice = input("Válasszon rendezési módot (növekvő/csökkenő): ").lower()
+if choice == 'növekvő':
+bubble_sort(data)
+break
+elif choice == 'csökkenő':
+bubble_sort(data, reverse=True)
+break
+else:
+print("Érvénytelen választás. Kérem, válasszon növekvő vagy csökkenő rendezés közül.")
 
-    while True:
-        print("\nVálassz rendezési algoritmust:")
-        print("1. Egyszerű cserés rendezés")
-        print("2. Buborékrendezés")
-        print("3. Beszúrásos rendezés")
-        print("4. Minimum/maximumkiválasztásos rendezés")
-        print("5. Saját rendezési algoritmus")
-        print("0. Kilépés")
-        choice = input("Választás (0-5): ")
+print("Rendezett adatok:", data)
 
-        if choice == "0":
-            break
-        elif choice in ("1", "2", "3", "4", "5"):
-            reverse = input("Növekvő (n) vagy csökkenő (c) rendezés? ").lower() == "c"
-
-            if choice == "1":
-                simple_swap_sort(data, reverse)
-            elif choice == "2":
-                bubble_sort(data, reverse)
-            elif choice == "3":
-                insertion_sort(data, reverse)
-            elif choice == "4":
-                selection_sort(data, reverse)
-            elif choice == "5":
-                custom_sort(data, reverse)
-
-            print("\nRendezés után:")
-            print(data)
-        else:
-            print("Érvénytelen választás!")
-
-    while True:
-        new_item = input("\nÚj elem hozzáadása (üres sor befejezi): ")
-        if not new_item:
-            break
-        if is_numeric:
-            if is_number(new_item):
-                data.append(float(new_item))
-            else:
-                print("Érvénytelen adat!")
-        else:
-            data.append(new_item)
-
-        print("\nAz adatok frissítése után:")
-        print(data)
-
-    with open(filename, 'w') as file:
-        for item in data:
-            file.write(str(item) + "\n")
-
-if __name__ == "__main__":
-    main()
+if name == "main":
+main()
